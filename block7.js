69e37dd901f90736666fd629a40e342fdf6edef4
Maui Bautista <mherdy_2@hotmail.com>
2023-03-20 23:12:18 -0700

    java

--- block7.js
+++ block7.js
@@ -1,9 +1,7 @@
-//
-console. log(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
 let one = 10;
 console. log(one + 5 - 5);
 let two = 4;
 console. log(two * 10);
 let three = 100;
-console. log(three / 2 - 11);
-
+console. log(three / 2 - 1);
