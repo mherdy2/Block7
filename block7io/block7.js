0393c186a2fdbe88733a3b1887b54d1f84e7d430
Maui Bautista <mherdy_2@hotmail.com>
2023-03-21 23:55:36 -0700

    jscode

---
+++ block7io/block7.js
@@ -0,0 +1,24 @@
+//1. user stepped isto a hidden vault
+//2. user saw the digital key entry
+//3. user types thes first code  and press the enter
+//4. user typed the second code and press the enter again
+//5. finally the vault is asking for the final code
+//6. user hear the confirm sound the the vault is open
+//7. user finally enters the vault
+
+//messager to unlock vault
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
+
+//adding 2 numbers equals to 10 for the first code
+let one = 5 + 5;
+alert(`Verifying...` +` ` + one);
+
+//multiplying 2 numbers to get the second code
+let two = 4 * 10;
+alert(`Verifying...` +` ` + two);
+
+//dividing 2 numbers then minus the the corresponding number for the third code
+let three = 100 / 2 - 11;
+alert(`Verifying...` +` ` + three);
+
+alert(`Successfull press OK to continue`);
