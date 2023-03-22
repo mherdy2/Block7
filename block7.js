f860b8deb3f442793061f2418500542e52051f6a
Maui Bautista <mherdy_2@hotmail.com>
2023-03-21 19:52:56 -0700

    java

--- block7.js
+++ block7.js
@@ -1,9 +1,12 @@
 //user about to reveal the vault code if pressing oOK on the pop up message
+//let message = (`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
 alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
-//1. creating formula for desired output
-var one = 10;
-console. log(one + 5 - 5);
-var two = 4;
-console. log(two * 10);
-var three = 100;
-console. log(three / 2 - 11);
+//  1. creating formula for desired output
+let one = 5 + 5;
+alert(one);
+
+let two = 4 * 10
+alert(two)
+
+let three = 100 / 2 - 11
+alert(three);
